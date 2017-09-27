1cb986c049196abe7727bf08fa54f66a6c4b41b8
Volkan Nazmi Metin <v.metin@triodor.eu>
2017-09-27 14:58:46 +0300

    Character and Skill management

--- domain/brain.js
+++ domain/brain.js
@@ -1,6 +1,7 @@
 'use strict';
+const config = require('../config');
 const synaptic = require('synaptic');
 const Architect = synaptic.Architect;
-module.exports = new Architect.LSTM(2, 4, 4, 4, 1);
+module.exports = new Architect.LSTM(config.BrainInputCount, 4, 4, 4, 1);
 
 
