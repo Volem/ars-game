3143b5ba4fc4a61bc04100f2b8ab85783072896d
Volkan Nazmi Metin <v.metin@triodor.eu>
2017-09-27 09:30:45 +0300

    character, inventory, item, trade

--- domain/location.js
+++ domain/location.js
@@ -1,8 +1,6 @@
 'use strict';
-const Location = function(x = 0, y = 0) {
+module.exports = function(x = 0, y = 0) {
 	this.x = x;
 	this.y = y;
-	return this;
 };
 
-module.exports = Location;
--- index.js
+++ index.js
@@ -3,22 +3,28 @@
 const Character = require('./domain/character');
 const Location = require('./domain/location');
 const Direction = require('./domain/direction');
+const Inventory = require('./domain/inventory');
 
 let Volem = new Character('Volem');
 let currentLocation = new Location();
+let volemsInventory = new Inventory();
 
 const mobileVolem = Volem.Move(7);
+const runningVolem = Volem.Run(7);
 
 const VolemMovesEast = mobileVolem(Direction.East);
 const VolemMovesWest = mobileVolem(Direction.West);
 const VolemMovesNorth = mobileVolem(Direction.North);
 const VolemMovesSouth = mobileVolem(Direction.South);
+const VolemRunsSouth = runningVolem(Direction.South);
 
 currentLocation = VolemMovesEast(currentLocation);
-console.log(`${Volem.name}'s new Location : ${JSON.stringify(currentLocation)}`);
+console.log(`${Volem.Name}'s new Location : ${JSON.stringify(currentLocation)}`);
 currentLocation = VolemMovesWest(currentLocation);
-console.log(`${Volem.name}'s new Location : ${JSON.stringify(currentLocation)}`);
+console.log(`${Volem.Name}'s new Location : ${JSON.stringify(currentLocation)}`);
 currentLocation = VolemMovesSouth(currentLocation);
-console.log(`${Volem.name}'s new Location : ${JSON.stringify(currentLocation)}`);
+console.log(`${Volem.Name}'s new Location : ${JSON.stringify(currentLocation)}`);
 currentLocation = VolemMovesNorth(currentLocation);
-console.log(`${Volem.name}'s new Location : ${JSON.stringify(currentLocation)}`);
+console.log(`${Volem.Name}'s new Location : ${JSON.stringify(currentLocation)}`);
+currentLocation = VolemRunsSouth(currentLocation);
+console.log(`${Volem.Name}'s new Location : ${JSON.stringify(currentLocation)}`);
