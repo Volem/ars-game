76429512585ecae17099ef53890be56edab2ac35
Volkan Nazmi Metin <v.metin@triodor.eu>
2017-10-03 09:45:39 +0300

    Event Handler

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
-let start = new Date();
-console.log(start);
 const _ = require('lodash');
 const items = require('./domain/itemcomposition');
 const produce = require('./service/produce');
 const calcPrice = require('./service/calculatePrice');
 const CharManager = require('./service/characterManager');
 const skills = require('./domain/skill');
+
+const EventHandler = require('./service/eventHandler');
 
 let volemsOres = Array(10).fill(items.Ore);
 let volemsWoods = Array(10).fill(items.Wood);
@@ -25,10 +25,13 @@ volem = createBlacksmith('Volem');
 volem.Inventory.Items = [...volemsOres, ...volemsWoods, items.Stick];
 console.log(`Produced Item : ${_.difference(produceHatchet(volem), volem.Inventory.Items)[0].Name}`);
 
-for(let a of Object.keys(items)){
+for (let a of Object.keys(items)) {
 	console.log(`Price of ${a} : ${calcPrice(items[a])}`);
 }
 
-let end = new Date();
-console.log(end);
-console.log(end-start);
+let testEH = new EventHandler('CharacterCreated', createBlacksmith);
+
+let cc = testEH('Name');
+
+
+volem = testEH('VolemWithEvent');
